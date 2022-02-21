204bf93a888247e68c466b87c05cf81453ccc2e6
shmelzz <shelemekhliza0313@gmail.com>
2022-02-21 20:22:29 +0300

    Lab 6 bonus done

---
+++ task5-3.c
@@ -0,0 +1,32 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+int main()
+{
+  int     fd[2];
+  int size_pipe=0;
+  char *c;
+
+  char  resstring[14];
+
+  if (pipe(fd) < 0) {
+    printf("Can\'t open pipe\n");
+    exit(-1);
+  }
+  
+  fcntl(fd[1], F_SETFL,O_NONBLOCK);
+
+	int size=1;
+	while(size==1){
+	   size=write(fd[1], "p", 1);
+	   if(size==0){
+		   break;
+		   }
+	   size_pipe+=1;
+	}
+	printf("размер: %d байт\n", size_pipe);
+  return 0;
+}
