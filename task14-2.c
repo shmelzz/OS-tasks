bc0c836d6a2d92354017dad738f7fa669e9df868
shmelzz <shelemekhliza0313@gmail.com>
2022-05-30 14:08:06 +0300

    Lab 14 done

---
+++ task14-2.c
@@ -0,0 +1,18 @@
+#include <signal.h>
+#include <stdio.h>
+
+void my_handler(int nsig) {
+  if(nsig == 2)
+     printf("Receive signal %d, CTRL-C pressed\n", nsig);
+  if(nsig == 3)
+     printf("Receive signal %d, CTRL-4 pressed\n", nsig);
+}
+
+int main(void) {
+  
+  (void)signal(SIGINT, my_handler);
+  (void)signal(SIGQUIT, my_handler);
+  
+  while(1);
+  return 0;
+}
